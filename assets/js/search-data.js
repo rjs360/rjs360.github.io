8e869952c0ae164194281cb2d40202c8e2cbf97b
rjs360 <rudero@gmail.com>
2026-03-22 23:22:08 +0000

    Deploying to gh-pages from @ rjs360/rjs360.github.io@7aed6f6e64a4c0f40bfde519906c93d41978a21c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -455,13 +455,13 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-molecular-biology",
-          title: 'molecular biology',
-          description: "another without an image",
+            },},{id: "projects-molecular-physiology",
+          title: 'molecular physiology',
+          description: "another with an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-others",
-          title: 'others',
+            },},{id: "projects-other",
+          title: 'other',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
