c43d50639bc340ce63fae21e10216afe59279e73
rjs360 <rudero@gmail.com>
2026-03-23 23:39:18 +0000

    Deploying to gh-pages from @ rjs360/rjs360.github.io@f3d871435adbd047c9d7031eced96f8be755d550 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -442,27 +442,27 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-mechanics",
           title: 'mechanics',
-          description: "with background image",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-energetics",
           title: 'energetics',
-          description: "a project with a background image and giscus comments",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-thermal-biology",
           title: 'thermal biology',
-          description: "a project that redirects to another website",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-pathogens",
           title: 'pathogens',
-          description: "another with an image",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-other",
           title: 'other',
-          description: "a project with a background image",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
